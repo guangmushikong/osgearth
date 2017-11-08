ce4031b595eaf37b9f4cf0b6109e1b1f43d52ff9
凃理根 <tuligen@navinfo.com>
2017-11-08 14:24:29 +0800

    调整三方库引用路径 用于移动盘编译

--- cmake_scripts.sh
+++ cmake_scripts.sh
@@ -1,4 +1,11 @@
-export THIRDPARTY_PATH=/Users/cateye/release/3rdParty
+#  
+#  cmake_script.sh
+#  cmake脚本文件(cmake 版本推荐3.0）
+#  Created by wangmiao on 17/11/7
+#  Copyright (c) 2017年 cateye All rights reserved.
+#  调整THIRDPARTY_PATH 用于移动盘路径      by  涂理根  17/11/7
+
+export THIRDPARTY_PATH=/Volumes/mac/Cateye/3rdParty
 /Applications/CMake.app/Contents/bin/cmake ./ -G Xcode -DOSGEARTH_BUILD_PLATFORM_IPHONE:BOOL=ON \
 -DIPHONE_SDKVER="10.3" \
 -DIPHONE_VERSION_MIN="8.0" \
